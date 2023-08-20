e6d1a479053c47f1b5fe17cc2273dad33fead200
Andrew Drane <ecstokely@gmail.com>
2023-08-20 08:01:18 -0500

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,24 +1,26 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
+//Do you have a valid token
+app.UseAuthentication();
+//The token is valid, what are you allowed to do
 app.UseAuthorization();
 
+
 app.MapControllers();
+
 
 app.Run();
